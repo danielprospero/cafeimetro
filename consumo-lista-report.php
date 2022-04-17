98d05eb28e6a9e21dd2cf00649bebe1ed5244d05
Daniel Prospero Ribeiro <danielprosperoribeiro@outlook.com.br>
2022-04-17 01:06:40 -0300

    Adicionando Relatório e totalizador.

--- consumo-lista-report.php
+++ consumo-lista-report.php
@@ -1,42 +1,62 @@
 <?php
-include 'cabecalho-menu.php';
+date_default_timezone_set('Brazil/East');
+
 include 'conecta.php';
 include 'consumo-banco.php';
+
+$valor_total = 0;
+$ml_total = 0;
+$cafe_total = 0;
+$data_hoje = date('d/m/Y H:m:s');
 ?>
-<h1>O que eu já consumir...</h1>
 
-<button class="btn btn-primary" onclick="window.location.href='consumo-form-adiciona.php'">Mais café</button></button>
-<button class="btn btn-secondary" onclick="window.open('consumo-lista-planilha.php', 'blank')">Exportar para planilha</button></button>
-<button class="btn btn-secondary" onclick="window.open('consumo-lista-report.php', 'blank')">Relatório</button></button> <br> <br>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cafeímetro</title>
+		<meta charset="utf-8">
+		<link href="css/bootstrap/bootstrap.css" rel="stylesheet" />
+		<link href="css/cafeimetro.css" rel="stylesheet" />
+	</head>
+	<body>
 
+		<button type="button" name="btn_criapdf" class="btn btn-secondary" onclick="geraPDF()">Gera PDF</button>
 
-	<table class="table table-striped table-bordered">
+		<div class="container" id="div-report">
+				<div class="principal">
+				<h3>Relatório de consumo em <?=$data_hoje?></h3>
+				<table class="table">
 
-		<?php
-		$consumos = listaConsumo($conexao);	
-		foreach ($consumos as $consumo)
-		{
-		?>
-			<tr>
-				<td><?=$consumo['data_consumo']?></td>
-				<td><?=$consumo['hora_consumo']?></td>
-				<td><?=$consumo['dia_semana']?></td>
-				<td><?=$consumo['cafe_nome']?></td>
-				<td><?=$consumo['qtd']?>ml</td>
-				<td>R$ <?=number_format($consumo['preco'],2,',','.')?></td>
-				<td>
-					<form name="form-remove" method="post" action="consumo-exclui.php">
-						<input type="hidden" name="id" value="<?=$consumo['consumo_id']?>" />
-						<button class="btn btn-danger">remove</button>
-					</form>
-				</td>
-			</tr>
-		<?php
-		}
-		?>	
+					<?php
+					$consumos = listaConsumo($conexao);	
+					foreach ($consumos as $consumo)
+					{
+						$valor_total += $consumo['preco'];
+						$ml_total += $consumo['qtd'];
+						$cafe_total++;
+					?>
+						<tr>
+							<td><?=$consumo['data_consumo']?></td>
+							<td><?=$consumo['hora_consumo']?></td>
+							<td><?=$consumo['dia_semana']?></td>
+							<td><?=$consumo['cafe_nome']?></td>
+							<td><?=$consumo['qtd']?>ml</td>
+							<td>R$ <?=number_format($consumo['preco'],2,',','.')?></td>
+						</tr>
+					<?php
+					}
+					?>	
+				</table>
+				<br>			
+				<h3>Totalizador</h3>
+				<table class="table table-striped table-bordered">
 
-	</table>
-				
-<?php
-include 'rodape.php';
-?>
+					<tr style="text-align: center">
+						<td>Cafés:</td><td><?=$cafe_total?></td>
+						<td>Qtd ml</td><td><?=$ml_total?></td>
+						<td>R$</td><td><?=number_format($valor_total,2,',','.')?></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</body>
